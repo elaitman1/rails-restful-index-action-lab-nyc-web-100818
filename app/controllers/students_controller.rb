a54d5b2bf7bf892be91f9aeb14d7f0b1b0b76a6c
Eric Laitman <ericlaitman@Erics-MacBook-Pro-2.local>
2018-11-01 20:39:34 -0400

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,9 @@
+class StudentsController < ApplicationController
+
+def show
+
+@students = Student.all 
+end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  
+
+
+
+  get 'students',
+
+  to: 'students#show'
+
+
+
 end
